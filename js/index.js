cddec3f68ca3bea9993ff4120384e3be9151a02d
JayChou <kvkens@qq.com>
2015-08-08 11:19:47 +0800

    add page

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+	$(".J_menu > li").on("click",function(){
+		var $this = $(this),
+			$li = $this,
+			indexTitle = "海尔商城  ",
+			url = $this.find("a").attr("data-url"),
+			$J_navtext = $(".navgater label"),
+			title = $this.text();
+			
+		if(!$li.hasClass("li-current")){
+			$li.siblings().removeClass("li-current") ;
+			$li.addClass("li-current");
+		}
+		indexTitle += title;
+		document.title = indexTitle;
+		$J_navtext.html("用户体验监控 &gt;"+title);
+		console.log(url);
+		history.pushState({title:indexTitle},indexTitle,url);
+		//history.replaceState({title:indexTitle},indexTitle,url);
+		$.get("./page/" + url);
+	});
+	$(".content").css("height",$(document).height()-200);
+});
