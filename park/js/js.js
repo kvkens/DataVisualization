47eea8ae90cbc18a775285d8eab984ae5b660f1a
kvkens <kvkens@qq.com>
2018-03-21 14:26:50 +0800

    add:park

---
+++ park/js/js.js
@@ -0,0 +1,7 @@
+// prepend()
+//开头插入
+// append($('.name1,.name2,.name3,.name4').clone())
+//末尾插入
+// jquery 文档操作
+//parseInt(Math.random() * 6) random() 
+//方法可返回介于 0 ~ 1 之间的一个随机数。parseInt解析一个字符串并返回一个整数。
